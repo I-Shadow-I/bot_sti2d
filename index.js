bcf2307b68f3b7f63b5507f8b987c47b82626c1c
I-Shadow-I <57918832+I-Shadow-I@users.noreply.github.com>
2020-03-25 15:01:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -527,7 +527,7 @@ function time(){
     horaire = h + ':' + m;
     console.log(day+ ' / ' + h + ':' + m);
 
-    var msg_channel = client.channels.cache.get("692067052831113260")
+    var msg_channel = client.channels.cache.get("692067052831113260");
     
     if(horaire ==="18:30"){
         const embed = new MessageEmbed()
